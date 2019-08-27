173410293bbe4a474df902c6dc700a525ca772c9
Julien Atanassian <julien.atanassian@gmail.com>
2019-08-27 17:41:21 +0000

    made a function that takes a sentence and displays it as an animation

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+const newSentence = sentence + "\n";
+let counter = 0;
+for (const char of newSentence) {
+  setTimeout( () => { 
+    process.stdout.write(char);
+  }, counter )
+  counter += 50;
+};
